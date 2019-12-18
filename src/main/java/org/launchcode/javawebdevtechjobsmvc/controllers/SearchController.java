0db77263f6fbf1858df8fc46c7af6dd965b29fc5
Dottie <dottiequick@gmail.com>
2019-12-18 16:46:54 -0600

    Completed Search Functionality

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -10,11 +10,15 @@ import java.util.ArrayList;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 /**
  * Created by LaunchCode
  */
 @Controller
-@RequestMapping("search")
+@RequestMapping(value = "search")
 public class SearchController {
 
     @RequestMapping(value = "")
@@ -24,5 +28,25 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+    @RequestMapping(value="results", method = {RequestMethod.GET, RequestMethod.POST})
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+
+        ArrayList<Job> jobs;
+        if (searchTerm.toLowerCase().equals("all")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 
 }
